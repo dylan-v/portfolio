9e5a839b1ba48b0c74e5cff1850f43f2aa394d4f
dgv3 <dgv3@duke.edu>
2019-12-21 18:36:57 -0500

    new design

--- src/components/App.js
+++ src/components/App.js
@@ -18,17 +18,16 @@ class App extends Component {
     return (
       
     <Router>
-        <div className="square"></div>
-        <div className="circle"></div>
-        <div className="container">
-        <div className="navigation">
-          {/* <img src={logo} className="logo-image" alt="Logo Image" /> */}
+        {/* <div className="square"></div>
+        <div className="circle"></div> */}
+        <div className="content">
+
+        {/* <div className="navigation">
           <div className="navigation-sub">               
-            {/* Set up the Links */}
             <Link to="/" className="item">Home</Link>
             <Link to="/about" className="item">About</Link>
           </div>
-        </div>
+        </div> */}
         
         {/* Set up the Router */}
         <Route exact path="/" component={Home} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,33 @@ import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Cover from './assets/images/Rectangle.jpg'
+import Logo from './assets/images/dd-logo.svg'
+import Plants from './assets/images/Home.svg'
+
 
 class Home extends Component {
   render() {
     return (
+      <div>
+        <div class="container">
+          <div class="row header-content">
+            <div class="col-lg-3 header-logo"><img className="header-logo" src={Logo} /></div>
+            <div class=" col-md-12">
+              <h1>Dylan Vanelli is a <span style={{color:'#DE5948'}}>UX Designer</span> using service design to streamline banking at Capital One.</h1>
+            </div>
+            <div class=" col-md-12 motto">
+              <h3><b>Motto: </b>“Make the right thing, make the thing right.”</h3>
+            </div>
+          </div>
+          </div>
+          <div class="col-lg-12"><img className="plants" src={Plants} /></div>
+
+        <div class="work-section">
         <div class="container">
           <div class="row">
             <div class="col-12">
-              <h1>I’m Dylan, a designer who makes products and services human-centered.</h1>
+              <h2> Selected works</h2>
             </div>
-            </div>
-            <div class="row">
             <div class="col-xl-8">
               <Thumbnail
                 link="/SynXis"
@@ -52,17 +68,11 @@ class Home extends Component {
               />
               </div>
               <div class="col-lg-4">
-
-              {/* <Thumbnail
-                link="/project3"
-                image={Cover}
-                title="Photoshop Redesign"
-                category="Desktop App"
-              /> */}
               </div>
           </div>
         </div>
-        
+        </div>
+    </div>
     );
   }
 }
